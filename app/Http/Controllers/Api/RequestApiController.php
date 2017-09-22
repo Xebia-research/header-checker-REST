a9a15c9198a96194752a013ab6ba6535108cd889
Casper Mons <casper.mons@hva.nl>
2017-09-22 12:53:57 +0200

    Moved non-controller logic to separate class @sjongejan

--- app/Http/Controllers/Api/RequestApiController.php
+++ app/Http/Controllers/Api/RequestApiController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-
 use App\Http\Controllers\Controller;
+use App\Parsers\HeaderParser;
 
 class RequestApiController extends Controller
 {
@@ -20,30 +20,12 @@ class RequestApiController extends Controller
     public function check($url)
     {
         //Retrieve the headers and store in an array
-        $header = $this->getAllHeaders($url);
+        $header = HeaderParser::getAllHeaders($url);
         return view
         ('master', [
             'url' => $url,
             'header' => $header
         ]);
-    }
-
-    // Function that retrieves the headers and returns them in form of an array
-    public function getAllHeaders($url)
-    {
-        // TODO: Make it possible to pass HyperTextTransferProtocol in url
-        // Include https in the url as it can currently not be included in the url field of the browser
-        // For now it is https by default
-        $url = 'https://' . $url;
-        // Retrieve headers and store in array
-        try {
-            $header = get_headers($url, 1);
-            return $header;
-        }
-        // If the url could not be found, throw new exception
-        catch (\Exception $exception) {
-            throw new \Exception("The given URL could not be resolved.");
-        }
     }
 }
 
