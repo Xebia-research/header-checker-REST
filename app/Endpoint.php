1217ddc0ec400d69e38ca6e8ec1c1552b9fa180b
Stefan Jongejan <contact@sjongejan.eu>
2017-09-22 16:29:13 +0200

    Removed wrongly added company comments

--- app/Endpoint.php
+++ app/Endpoint.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Company: Sjongejan Development
- * User: Stefan Jongejan
- * Date: 22-09-2017
- * Time: 11:44
- */
 
 namespace App;
 
--- app/Response.php
+++ app/Response.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Company: Sjongejan Development
- * User: Stefan Jongejan
- * Date: 22-09-2017
- * Time: 11:45
- */
 
 namespace App;
 
