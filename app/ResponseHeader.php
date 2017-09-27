e7d024b81e64982a92efb3bf53f06cada3b53735
Casper Mons <casper.mons@hva.nl>
2017-09-27 15:36:35 +0200

    Added reverse Relationships to Response and ResponseHeaders

--- app/ResponseHeader.php
+++ app/ResponseHeader.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ResponseHeader extends Model
 {
-    //
+    public function reponse(): BelongsTo
+    {
+        return $this->belongsTo(Response::class);
+    }
 }
