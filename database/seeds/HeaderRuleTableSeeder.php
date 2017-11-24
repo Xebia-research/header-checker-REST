c81052c3d6eb068cb005fda6acd0324ce4c6a4bc
Stefan Jongejan <stefan.jongejan@hva.nl>
2017-11-24 10:01:35 +0100

    Added HeaderRuleTableSeeder and ProfileTableSeeder

---
+++ database/seeds/HeaderRuleTableSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\HeaderRule;
+use Illuminate\Database\Seeder;
+
+class HeaderRuleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('header_rules')->truncate();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'Strict-Transport-Security';
+        $headerRule->validation_type = 'include';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'Strict-Transport-Security';
+        $headerRule->validation_type = 'regex';
+        $headerRule->validation_value = '^max-age=[0-9]+(; includeSubDomains)?(; preload)?$';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'Public-Key-Pins';
+        $headerRule->validation_type = 'include';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'Public-Key-Pins';
+        $headerRule->validation_type = 'regex';
+        $headerRule->validation_value = '^pin-sha256="[a-zA-Z0-9=+]{44}"(; pin-sha256="[a-zA-Z0-9=+]{44}")*(; report-uri="((?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&\'\(\)\*\+,;=.]+)")(; max-age=[0-9]+)(; includeSubDomains)$';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'X-Frame-Options';
+        $headerRule->validation_type = 'include';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+
+        $headerRule = new HeaderRule;
+        $headerRule->name = 'X-Frame-Options';
+        $headerRule->validation_type = 'regex';
+        $headerRule->validation_value = '^DENY|SAMEORIGIN|ALLOW-FROM: ((?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&\'\(\)\*\+,;=.]+)$';
+        $headerRule->risk_level = 'low'; // Todo
+        $headerRule->save();
+    }
+}
