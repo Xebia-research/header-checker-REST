7719fa73d341f55f9449734d70e23e6a95b6f54b
Fabian de Laender <fabian.de.laender@gmail.com>
2017-12-01 13:12:44 +0100

    Add 'Request' API resource (collection) classes for JSON, XML, and HTML

---
+++ app/Http/Resources/Html/RequestCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Html;
+
+use App\Http\Resources\Types\Html\ResourceCollection;
+
+class RequestCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
