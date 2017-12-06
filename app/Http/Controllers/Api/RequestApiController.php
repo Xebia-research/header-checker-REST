f3be9c3d609757c136295f79c7e80ec34e8b4419
Stefan Jongejan <stefan.jongejan@hva.nl>
2017-12-06 10:51:32 +0100

    Moved converting resource response to api controller

--- app/Http/Controllers/Api/RequestApiController.php
+++ app/Http/Controllers/Api/RequestApiController.php
@@ -8,14 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Jobs\ExecuteRequestJob;
 use App\Request as EndpointRequest;
-use App\Http\Controllers\Controller;
 
 class RequestApiController extends Controller
 {
-    private const FORMAT_XML = 'xml';
-    private const FORMAT_JSON = 'json';
-    private const FORMAT_HTML = 'html';
-
     /**
      * Find all requests in the database.
      *
@@ -121,37 +116,5 @@ class RequestApiController extends Controller
         $endpointRequest->requestHeaders()->createMany($request->get('request_headers', []));
 
         return $endpointRequest;
-    }
-
-    /**
-     * @param  mixed  $resource
-     * @param  null|string  $format
-     * @return \App\Http\Resources\Html\Request|\App\Http\Resources\Json\Request|\App\Http\Resources\Xml\Request
-     */
-    protected function singleResourceResponse($resource, ?string $format)
-    {
-        if ($format == static::FORMAT_HTML) {
-            return new \App\Http\Resources\Html\Request($resource);
-        } elseif ($format == static::FORMAT_XML) {
-            return new \App\Http\Resources\Xml\Request($resource);
-        } else {
-            return new \App\Http\Resources\Json\Request($resource);
-        }
-    }
-
-    /**
-     * @param  mixed  $resources
-     * @param  null|string  $format
-     * @return \App\Http\Resources\Html\RequestCollection|\App\Http\Resources\Json\RequestCollection|\App\Http\Resources\Xml\RequestCollection
-     */
-    protected function resourceCollectionResponse($resources, ?string $format)
-    {
-        if ($format == static::FORMAT_HTML) {
-            return new \App\Http\Resources\Html\RequestCollection($resources);
-        } elseif ($format == static::FORMAT_XML) {
-            return new \App\Http\Resources\Xml\RequestCollection($resources);
-        } else {
-            return new \App\Http\Resources\Json\RequestCollection($resources);
-        }
     }
 }
